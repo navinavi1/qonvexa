34da8a3353fbd2e0fa5b930cb2e88c425267f35c
navinavi1 <navi.navi1@ukr.net>
2026-09-07 22:27:57 +0300

    feat: use search-first worldwide application routing

--- scripts/start-autonomos.mjs
+++ scripts/start-autonomos.mjs
@@ -2,7 +2,7 @@ import 'dotenv/config';
 import { InternetHunter } from '../src/autonomos/internet-hunter.js';
 import { GlobalWorkHunter } from '../src/autonomos/global-work-hunter.js';
 import { ReliableGlobalLeadActioner } from '../src/autonomos/reliable-global-lead-actioner.js';
-import { BrowserlessLeadActioner } from '../src/autonomos/browserless-lead-actioner.js';
+import { SearchFirstLeadActioner } from '../src/autonomos/search-first-lead-actioner.js';
 import { migrateGlobalActionerState } from '../src/autonomos/global-actioner-migrations.js';
 import { TaskForceVerifier } from '../src/autonomos/taskforce-verifier.js';
 import { TaskForceWorker } from '../src/autonomos/taskforce-worker.js';
@@ -26,7 +26,7 @@ const globalHunter=new GlobalWorkHunter({env:process.env,storageDir:process.env.
 const browserActioner=enabled(process.env.AUTONOMOS_GLOBAL_ACTIONER_ENABLED,'false')
   ? new ReliableGlobalLeadActioner({env:process.env,storageDir:process.env.STORAGE_DIR,logger:console}) : null;
 const browserlessActioner=enabled(process.env.AUTONOMOS_BROWSERLESS_ACTIONER_ENABLED,'true')
-  ? new BrowserlessLeadActioner({env:process.env,storageDir:process.env.STORAGE_DIR,logger:console}) : null;
+  ? new SearchFirstLeadActioner({env:process.env,storageDir:process.env.STORAGE_DIR,logger:console}) : null;
 const taskForceVerifier=new TaskForceVerifier({env:process.env,storageDir:process.env.STORAGE_DIR,logger:console});
 const taskForceWorker=new TaskForceWorker({env:process.env,storageDir:process.env.STORAGE_DIR,logger:console});
 const globalFeedPublisher=new GlobalFeedPublisher({env:process.env,storageDir:process.env.STORAGE_DIR,logger:console});
