0f3e7ec041f6e1565a68cf3a6afcad65e028314d
navinavi1 <navi.navi1@ukr.net>
2026-09-07 20:01:50 +0300

    feat: boot global work hunter and TaskForce agent

--- scripts/start-autonomos.mjs
+++ scripts/start-autonomos.mjs
@@ -1,9 +1,15 @@
 import 'dotenv/config';
 import { InternetHunter } from '../src/autonomos/internet-hunter.js';
+import { GlobalWorkHunter } from '../src/autonomos/global-work-hunter.js';
 
 const hunter=new InternetHunter({env:process.env,storageDir:process.env.STORAGE_DIR,logger:console});
+const globalHunter=new GlobalWorkHunter({env:process.env,storageDir:process.env.STORAGE_DIR,logger:console});
+
 hunter.start();
-process.on('SIGTERM',()=>hunter.stop());
-process.on('SIGINT',()=>hunter.stop());
+globalHunter.start();
+
+const stop=()=>{hunter.stop();globalHunter.stop();};
+process.on('SIGTERM',stop);
+process.on('SIGINT',stop);
 
 await import('../server.js');
